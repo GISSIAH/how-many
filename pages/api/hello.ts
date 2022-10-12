7781e42c00da9002b9aefd245e4029a171b288b7
Bryan Malunje <62018768+GISSIAH@users.noreply.github.com>
2022-10-12 13:46:21 +0200

    Fix: removed mongo connector

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,19 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { connectToDatabase } from '../../db/mongo';
-
-type Data = {
-  name: string
-}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  const { client,db } = await connectToDatabase();
   
-  const test = await db.collection("movies")
-  .find({})
-  .toArray();
-  res.status(200).json(test)
+  res.send({name:"hello world"})
 }
